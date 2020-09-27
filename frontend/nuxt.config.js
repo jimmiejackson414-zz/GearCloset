170123bd9a8b2475008189ef0396f38f5202e5bc
Jimmie Jackson <jjackson@1800accountant.com>
2020-09-27 11:01:17 -0700

    initial nuxt commit

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,68 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  head: {
+    titleTemplate: '%s - GearCloset',
+    title: 'GearCloset',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  css: [
+  ],
+
+  plugins: [
+    { src: '~/plugins/vue-unicons', mode: 'client' },
+  ],
+
+  components: true,
+
+  buildModules: [
+    '@nuxtjs/vuetify',
+  ],
+
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/auth',
+    '@nuxtjs/pwa',
+  ],
+
+  auth: {
+    strategies: {
+      'laravel.passport': {
+        url: '',
+        client_id: '',
+        client_secret: '',
+      }
+    }
+  },
+
+  axios: {},
+
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  build: {
+  }
+}
---
+++ frontend/plugins/vue-unicons.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import Unicon from 'vue-unicons';
+import {
+  uniAngleLeft,
+  uniAngleRight,
+  uniAngleUp,
+  uniCheckCircle,
+  uniEnvelopeAlt,
+  uniExpandArrowsAlt,
+  uniFacebookF,
+  uniImageDownload,
+  uniInfoCircle,
+  uniInstagramAlt,
+  uniMultiply,
+  uniSearch
+} from 'vue-unicons/src/icons';
+
+Unicon.add([
+  uniAngleLeft,
+  uniAngleRight,
+  uniAngleUp,
+  uniCheckCircle,
+  uniEnvelopeAlt,
+  uniExpandArrowsAlt,
+  uniFacebookF,
+  uniImageDownload,
+  uniInfoCircle,
+  uniInstagramAlt,
+  uniMultiply,
+  uniSearch
+]);
+Vue.use(Unicon);
